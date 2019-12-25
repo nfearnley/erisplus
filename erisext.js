336ac0518fd00eb97d4f23c9b5c0940e7d8de2c6
Natalie Fearnley <nfearnley@gmail.com>
2019-12-24 23:48:39 -0500

    Trying to get extensions to load

---
+++ erisext.js
@@ -0,0 +1,120 @@
+const path = require("path");
+const fs = require("fs");
+
+const requireReload = require("require-reload")(require);
+const callerModule = require("caller-module");
+
+function setupExtension(bot, extpath) {
+    bot.extensions = bot.extensions || {};
+
+    if (bot.extensions[extpath]) {
+        throw new Error(`Extension ${extpath} already loaded.`);
+    }
+
+    if (extpath) {
+        throw new Error(`Extension ${extpath} not found.`);
+    }
+
+    var extension = requireReload(extpath);
+
+    extension.setup(bot);
+
+    bot.extensions[extpath] = extension;
+
+    return extension;
+}
+
+function teardownExtension(callerbot, extpath) {
+    bot.extensions = bot.extensions || {};
+
+    if (!bot.extensions[extpath]) {
+        throw new Error(`Extension ${extpath} not yet loaded.`);
+    }
+
+    bot.extensions[extpath].teardown(bot);
+
+    delete bot.extensions[extpath];
+}
+
+/**
+ * Load an extension
+ *
+ * @memberOf external:Client#
+ * @method loadExtension
+ * @arg {string} extname
+ */
+Object.defineProperty(Eris.Client.prototype, "loadExtension", {
+    value: function(extname) {
+        var bot = this;
+
+        var extpath = path.resolve([callerModule().path, extname]);
+
+        setupExtension(bot, extpath);
+
+        return bot;
+    }
+});
+
+/**
+ * Reload an extension
+ *
+ * @memberOf external:Client#
+ * @method reloadExtension
+ * @arg {string} extname
+ */
+Object.defineProperty(Eris.Client.prototype, "reloadExtension", {
+    value: function(extname) {
+        var bot = this;
+
+        var extpath = path.resolve([callerModule().path, extname]);
+
+        teardownExtension(bot, extpath);
+        setupExtension(bot, extpath);
+
+        return bot;
+    }
+});
+
+/**
+ * Unload an extension
+ *
+ * @memberOf external:Client#
+ * @method unloadExtension
+ * @parameter {string} extname
+ */
+Object.defineProperty(Eris.Client.prototype, "unloadExtension", {
+    value: function(extname) {
+        var bot = this;
+
+        var extpath = path.resolve([callerModule().path, extname]);
+
+        teardownExtension(bot, extpath);
+
+        return bot;
+    }
+});
+
+/**
+ * Load all extensions in a directory
+ *
+ * @memberOf external:Client#
+ * @method unloadExtension
+ * @arg {string} extdir
+ */
+Object.defineProperty(Eris.Client.prototype, "loadExtensions", {
+    value: function(extdir) {
+        var bot = this;
+
+
+        var extdirpath = path.resolve([callerModule().path, extdir]);
+
+        var files = fs.readdirSync(extdirpath);
+        files = files.filter(f => path.extname(f) === ".js");
+        files = files.map(f => path.resolve([extdirpath, f]));
+        files.foreach(function(extpath) {
+            setupExtension(bot, extpath);
+        });
+
+        return bot;
+    }
+});
